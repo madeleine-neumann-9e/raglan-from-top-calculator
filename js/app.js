38501b6eb0fdcf5417418488b9213538e8435482
Madeleine Neumann <madeleine.neumann@9elements.com>
2023-02-16 13:51:56 +0100

    additional improvements

--- js/app.js
+++ js/app.js
@@ -15,8 +15,8 @@ function calculate(e) {
     <h3 class="mb-4">You need:</h3>
     <p> Stiches Back: ${stichesEachPart} </p>
     <p>  Stiches Front: ${stichesEachPart} </p>
-    <p>  Stiches each Sleave: ${stichesEachPart / 2} </p>
-    <p>  Stiches each Raglan line: ${raglanLine} (before and after the 13 sleave stiches each)</p>
+    <p>  Stiches each Sleave: ${Math.round(stichesEachPart / 2)} </p>
+    <p>  Stiches each Raglan line: ${raglanLine} (before and after the ${Math.round(stichesEachPart / 2)} sleave stiches each)</p>
     <p>  Total Stiches to cast on: ${totalNumberOfStiches}</p>
   `;
 
